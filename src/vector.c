2c19feaac9bea2aac8ab921a854cfc1777c1d1b1
Aivan <aish3n@pm.me>
2022-10-02 12:57:14 +0800

    feat: implement vector

---
+++ src/vector.c
@@ -0,0 +1,96 @@
+// Copyright (c) 2022 Aishen
+//
+// MIT LICENSE
+// https://github.com/aishenreemo/libcollections/blob/master/license.md
+
+// idk how to name things forgive me xd
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "vector.h"
+
+// private struct/enum declarations
+
+// private function declarations
+
+// public function implementations
+void vector_init(vector_t *vec, vector_result_t *err) {
+	vec->length = 0;
+	vec->items = calloc(sizeof(void *), 1);
+
+	if (err != NULL) *err = (vec->items == NULL)
+		? VECTOR_RESULT_INIT_CALLOC_FAILURE
+		: VECTOR_RESULT_SUCCESS;
+}
+
+void *vector_get(vector_t *vec, uint index, vector_result_t *err) {
+	if (vec == NULL) {
+		if (err != NULL) *err = VECTOR_RESULT_UNDEFINED;
+		return NULL;
+	} else if (index < 0 || index >= vec->length) {
+		if (err != NULL) *err = VECTOR_RESULT_GET_INDEX_OVERLAP;
+		return NULL;
+	} else {
+		if (err != NULL) *err = VECTOR_RESULT_SUCCESS;
+		return vec->items[index];
+	}
+}
+
+void vector_push(vector_t *vec, void *item, vector_result_t *err) {
+	if (vec == NULL) {
+		if (err != NULL) *err = VECTOR_RESULT_UNDEFINED;
+		return;
+	} else if (item == NULL) {
+		if (err != NULL) *err = VECTOR_RESULT_PUSH_ITEM_INVALID;
+		return;
+	}
+
+	void **items = realloc(vec->items, sizeof(void *) * (vec->length + 1));
+
+	if (items == NULL) {
+		if (err != NULL) *err = VECTOR_RESULT_PUSH_REALLOC_FAILURE;
+		printf("?\n");
+		return;
+	}
+
+	items[vec->length] = item;
+	vec->length += 1;
+	vec->items = items;
+
+	if (err != NULL) *err = VECTOR_RESULT_SUCCESS;
+}
+
+void vector_remove(vector_t *vec, uint index, vector_result_t *err) {
+	if (vec == NULL) {
+		if (err != NULL) *err = VECTOR_RESULT_UNDEFINED;
+		return;
+	} else if (index < 0 || index >= vec->length) {
+		if (err != NULL) *err = VECTOR_RESULT_REMOVE_INDEX_OVERLAP;
+		return;
+	}
+
+	void **items = realloc(vec->items, sizeof(void *) * vec->length);
+	if (items == NULL) {
+		if (err != NULL) *err = VECTOR_RESULT_REMOVE_REALLOC_FAILURE;
+		return;
+	}
+
+	free(items[index]);
+	for (uint i = index; (i + 1) < vec->length; i++) {
+		items[i] = items[i + 1];
+	}
+
+	vec->length -= 1;
+	vec->items = items;
+
+	if (err != NULL) *err = VECTOR_RESULT_SUCCESS;
+}
+
+void vector_drop(vector_t *vec) {
+	if (vec == NULL) return;
+
+	vec->length = 0;
+	for (uint i = 0; i < vec->length; i++) free(vec->items[i]);
+	free(vec->items);
+}
