31c4406ed1e0bfe3a5be271a597586c837bce511
Aivan <aish3n@pm.me>
2022-10-02 12:56:09 +0800

    refactor: shorten declarations

--- include/vector.h
+++ include/vector.h
@@ -27,34 +27,16 @@ enum __vector_result_t__ {
 typedef struct __vector_t__        vector_t;
 typedef enum   __vector_result_t__ vector_result_t;
 
-void vector_init(
-	vector_t        *,
-	vector_result_t *
-);
-void vector_get(
-	vector_t        *,
-	unsigned int     ,
-	void            *,
-	vector_result_t *
-);
+// public functions declarations
 
-void vector_push(
-	vector_t        *,
-	void            *,
-	vector_result_t *
-);
+void vector_init(vector_t *, vector_result_t *);
+void *vector_get(vector_t *, unsigned int, vector_result_t *);
+void vector_push(vector_t *, void *, vector_result_t *);
 
-void vector_remove(
-	vector_t        *,
-	int              ,
-	vector_result_t *
-);
+void vector_remove(vector_t *, unsigned int, vector_result_t *);
 
-void vector_drop(
-	vector_t        *,
-	vector_result_t *
-);
+void vector_drop(vector_t *);
 
-void vector_print_error(vector_result_t);
+// void vector_print_error(vector_result_t);
 
 #endif // __COLLECTIONS_VECTOR_H__
